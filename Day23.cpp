f900ac50d231be683de3378552ae46cd491dffdd
Aman Kumar Upadhyay <aman08023010@gmail.com>
2024-03-23 17:24:07 +0530

    Blackjack (codechef problem)

---
+++ Day23.cpp
@@ -0,0 +1,34 @@
+/*
+Blackjack
+Chef is playing a variant of Blackjack, where 3 numbers are drawn and each number lies between 1 and 10 (with both 1 and 10 inclusive). 
+Chef wins the game when the sum of these 3 numbers is exactly 21.
+
+Given the first two numbers 
+A and B, that have been drawn by Chef, what should be 3-rd number that should be drawn by the Chef in order to win the game?
+Note that it is possible that Chef cannot win the game, no matter what is the 3-rd number. In such cases, report −1 as the answer.
+
+Input Format
+The first line will contain an integer T - number of test cases. Then the test cases follow.
+The first and only line of each test case contains two integers A and B - the first and second number drawn by the Chef.
+
+Output Format
+For each testcase, output the 3-rd number that should be drawn by the Chef in order to win the game. 
+Output −1 if it is not possible for the Chef to win the game.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,a,b,c;
+	cin>>t;
+	while(t--){
+	    cin>>a>>b;
+	    c=21-(a+b);
+	    if(c>10){
+	        cout<<-1<<endl;
+	    }else{
+	        cout<<c<<endl;
+	    }
+	}
+
+}
